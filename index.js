397de49ad86f813b08559d8ab66b6ae732255fe1
DanielSheehan <daniel.sheehan81@yahoo.com>
2021-10-19 10:46:52 -0400

    commit tree badccf765ad46f116aa535c20dc0d12cb1df7d58

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     let change = document.GetElementById('text')
+//     change.textContent = 'This is really cool!'
+//     console.log(change)
+// });
+
+document.addEventListener("DOMContentLoaded",() => {
+    let changes = document.getElementById('text');
+    changes.textContent = 'This is really cool!'
+});
